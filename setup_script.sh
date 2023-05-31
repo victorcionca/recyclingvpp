8cb8e379ef10207193bcbee5d543c0a67050de19
Jamie <jcotter1998@hotmail.com>
2023-05-31 23:55:45 +0100

    Install script setup

---
+++ setup_script.sh
@@ -0,0 +1,6 @@
+apt install python3-pip
+apt install libopenjp2-7
+apt install libatlas-base-dev
+apt install iperf3
+apt install git
+python3 -m pip install -r /home/pi/recyclingvpp/requirements.txt
