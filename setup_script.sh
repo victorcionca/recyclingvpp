f15542268b58b3571f381641d5779434b1336dd5
Jamie <jcotter1998@hotmail.com>
2023-06-01 02:32:52 +0100

    Modified setup script to change to user for pip

--- setup_script.sh
+++ setup_script.sh
@@ -3,4 +3,4 @@ apt install libopenjp2-7
 apt install libatlas-base-dev
 apt install iperf3
 apt install git
-python3 -m pip install -r /home/pi/recyclingvpp/requirements.txt
+sudo su - pi -c "python3 -m pip install -r /home/pi/recyclingvpp/requirements.txt"
