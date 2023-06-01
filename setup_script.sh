8b63fc015c71571cb8930a91cdc485914ce05d76
Jamie <jcotter1998@hotmail.com>
2023-06-01 02:39:34 +0100

    Forgot to include ntp

--- setup_script.sh
+++ setup_script.sh
@@ -3,4 +3,6 @@ apt install libopenjp2-7
 apt install libatlas-base-dev
 apt install iperf3
 apt install git
+apt install ntp
 sudo su - pi -c "python3 -m pip install -r /home/pi/recyclingvpp/requirements.txt"
+cp "/home/pi/recyclingvpp/ntp.conf" "/etc/"
