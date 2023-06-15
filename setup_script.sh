2f791557dd26901a37542d847d4bd23c7c6f2e1a
Jamie Cotter <39841865+JamieC1998@users.noreply.github.com>
2023-06-15 15:58:55 +0100

    Update setup_script.sh

--- setup_script.sh
+++ setup_script.sh
@@ -4,4 +4,5 @@ apt install libopenjp2-7
 apt install libatlas-base-dev
 apt install iperf3
 apt install git
+cp /home/pi/recyclingvpp/ntp.conf /etc/
 sudo su - pi -c "python3 -m pip install -r /home/pi/recyclingvpp/requirements.txt"
